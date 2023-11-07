dd64e748cec56b302a8eae02bc60c58ab5109731
amatov <6277757+amatov@users.noreply.github.com>
2023-11-06 19:43:15 -0800

    Add files via upload

---
+++ avgV_VHL_analysis_V13.R
@@ -0,0 +1,120 @@
+d  <- scan("C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl13.txt");
+data <- Mclust(d,3,3);
+class <- data$classification
+cat(class, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl13_classification_3G.txt");
+G <- data$G
+mu <- data$mu
+sigmasq <- data$sigmasq
+pro <- data$pro
+cat(G, mu, sigmasq, pro, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl13_distribution_3G.txt");
+
+d  <- scan("C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl12.txt");
+data <- Mclust(d,3,3);
+class <- data$classification
+cat(class, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl12_classification_3G.txt");
+G <- data$G
+mu <- data$mu
+sigmasq <- data$sigmasq
+pro <- data$pro
+cat(G, mu, sigmasq, pro, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl12_distribution_3G.txt");
+
+d  <- scan("C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl11.txt");
+data <- Mclust(d,3,3);
+class <- data$classification
+cat(class, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl11_classification_3G.txt");
+G <- data$G
+mu <- data$mu
+sigmasq <- data$sigmasq
+pro <- data$pro
+cat(G, mu, sigmasq, pro, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl11_distribution_3G.txt");
+
+d  <- scan("C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl10.txt");
+data <- Mclust(d,3,3);
+class <- data$classification
+cat(class, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl10_classification_3G.txt");
+G <- data$G
+mu <- data$mu
+sigmasq <- data$sigmasq
+pro <- data$pro
+cat(G, mu, sigmasq, pro, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl10_distribution_3G.txt");
+
+d  <- scan("C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl09.txt");
+data <- Mclust(d,3,3);
+class <- data$classification
+cat(class, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl09_classification_3G.txt");
+G <- data$G
+mu <- data$mu
+sigmasq <- data$sigmasq
+pro <- data$pro
+cat(G, mu, sigmasq, pro, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl09_distribution_3G.txt");
+
+d  <- scan("C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl08.txt");
+data <- Mclust(d,3,3);
+class <- data$classification
+cat(class, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl08_classification_3G.txt");
+G <- data$G
+mu <- data$mu
+sigmasq <- data$sigmasq
+pro <- data$pro
+cat(G, mu, sigmasq, pro, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl08_distribution_3G.txt");
+
+d  <- scan("C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl07.txt");
+data <- Mclust(d,3,3);
+class <- data$classification
+cat(class, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl07_classification_3G.txt");
+G <- data$G
+mu <- data$mu
+sigmasq <- data$sigmasq
+pro <- data$pro
+cat(G, mu, sigmasq, pro, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl07_distribution_3G.txt");
+
+d  <- scan("C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl06.txt");
+data <- Mclust(d,3,3);
+class <- data$classification
+cat(class, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl06_classification_3G.txt");
+G <- data$G
+mu <- data$mu
+sigmasq <- data$sigmasq
+pro <- data$pro
+cat(G, mu, sigmasq, pro, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl06_distribution_3G.txt");
+
+d  <- scan("C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl05.txt");
+data <- Mclust(d,3,3);
+class <- data$classification
+cat(class, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl05_classification_3G.txt");
+G <- data$G
+mu <- data$mu
+sigmasq <- data$sigmasq
+pro <- data$pro
+cat(G, mu, sigmasq, pro, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl05_distribution_3G.txt");
+
+d  <- scan("C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl04.txt");
+data <- Mclust(d,3,3);
+class <- data$classification
+cat(class, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl04_classification_3G.txt");
+G <- data$G
+mu <- data$mu
+sigmasq <- data$sigmasq
+pro <- data$pro
+cat(G, mu, sigmasq, pro, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl04_distribution_3G.txt");
+
+d  <- scan("C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl03.txt");
+data <- Mclust(d,3,3);
+class <- data$classification
+cat(class, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl03_classification_3G.txt");
+G <- data$G
+mu <- data$mu
+sigmasq <- data$sigmasq
+pro <- data$pro
+cat(G, mu, sigmasq, pro, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl03_distribution_3G.txt");
+
+
+d  <- scan("C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl02.txt");
+data <- Mclust(d,3,3);
+class <- data$classification
+cat(class, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl02_classification_3G.txt");
+G <- data$G
+mu <- data$mu
+sigmasq <- data$sigmasq
+pro <- data$pro
+cat(G, mu, sigmasq, pro, file = "C:/Alex/R/R-2.3.1/avgV_786OVHL30_NaCl02_distribution_3G.txt");
